c4f23160dfc5ca1a7f3182b91893a360d071d199
Mahmood83367 <Mahmood94abdelbary@gmail.com>
2021-05-25 19:47:57 +0300

    add class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Hello from "./component/Hello"
+import Welcome from "./component/Welcome"
+
 function App() {
   return (
     <div className="App">
         <Hello />
-        
+        <Welcome />
     </div>
   );
 }
